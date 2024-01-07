8c6486076b7a38613d1c9ed0d19bbc643d19de1d
Mojid Eroje <majidy287@gmail.com>
2024-01-07 11:52:06 +0000

    first commit

---
+++ Graphs-in_Python.py
@@ -0,0 +1,46 @@
+# Importing the relevant modules
+import matplotlib.pyplot as plt
+# # Plotting graph in Python
+# x = [1, 3, 5, 10] # This is what we are plotting
+# plt.plot(x)
+# # plt.show() # This will show the plot we are plotting
+#
+# # Plotting x and y against each other
+# y = [7, 12, 21, 22]
+# plt.plot(x, y)
+# # plt.show()
+
+# Lets plot something lovely
+
+# Line 1 - Points
+x = [3, 9, 14]
+y = [2, 7, 30]
+# Plotting x and y
+plt.plot(x, y, c="red", linewidth=2, label="Line 1")
+
+# Line 2 - Points
+x2 = [1, 15, 18]
+y2 = [0, 3, 12]
+# Plotting x2 and y2
+plt.plot(x2, y2, c="green", linewidth=2, label="Line 2", linestyle="dashed",
+         marker='s', markerfacecolor="orange", markersize=10)
+
+# Label the axes and give the plot a title
+plt.xlabel("Shoe sizes")
+plt.ylabel("Age")
+plt.title("Two line!")
+
+# Limits of the axis
+# plt.ylim(1, 10)
+# plt.xlim(0, 30)
+
+# Show the legend on the plot
+plt.legend()
+
+# Get python to show the p[lots
+plt.show()
+
+
+
+
+
