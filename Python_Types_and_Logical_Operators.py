dfb66a614443663a67bbddde9a6d2c94e5d81792
Mojid Eroje <majidy287@gmail.com>
2024-01-08 02:19:15 +0000

    third commit

--- Python_Types_and_Logical_Operators.py
+++ Python_Types_and_Logical_Operators.py
@@ -13,3 +13,13 @@ print (4.05, int(4.05))
 # Rounding Up!
 print(4.72, int(4.72), int(round(4.72)))
 
+# Logical operators
+# There are three different logical operators; 'and', 'or', 'not'
+
+x = 6
+print(x > 0 and x < 10)  # and means both must hold
+print(0 < x < 10)
+
+y = 9
+print(y % 2 == 0 or y % 3 == 0)  # or means one must hold
+
